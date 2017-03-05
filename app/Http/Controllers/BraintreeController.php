f2ac8dfb5eaf2df1a64af262df34dffe6495af08
Victor Lam <victorebox@yahoo.com.hk>
2017-03-06 03:06:07 +0800

    persist order

--- app/Http/Controllers/BraintreeController.php
+++ app/Http/Controllers/BraintreeController.php
@@ -4,6 +4,8 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+
+use App\Order;
 use App\BraintreeService;
 
 class BraintreeController extends Controller
@@ -15,6 +17,21 @@ class BraintreeController extends Controller
 
   public function create(Request $request)
   {
-    return (new BraintreeService())->createTransaction($request->input('nonce'));
+    $result = (new BraintreeService())->createTransaction(
+      $request->input('nonce'),
+      $request->input('amount')
+    );
+
+    $order = Order::create([
+      'customer_name' => $request->input('customerName'),
+      'customer_tel'  => $request->input('tel'),
+      'currency'      => $request->input('currency'),
+      'amount'        => $request->input('amount'),
+    ]);
+
+    return response()->json([
+      'transaction_id' => $result->transaction->id,
+      'order_id' => $order->id,
+    ]);
   }
 }
